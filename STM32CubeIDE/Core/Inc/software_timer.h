494eb87d5f145a088182458f1a6e3b0aebe10d44
Le Duc Cuong <leduccuonghcmut@gmail.com>
2024-10-28 08:46:31 +0700

    update main

---
+++ STM32CubeIDE/Core/Inc/software_timer.h
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer_flag[10];
+
+void setTimer(int i, int duration);
+void timer_run(int i);
+void timerRun();
+
+
+#endif
---
+++ STM32CubeIDE/Core/Src/software_timer.c
@@ -0,0 +1,24 @@
+#include "software_timer.h"
+#include "main.h"
+
+
+int timer_counter[10] = {0};
+int timer_flag[10] = {0};
+
+
+void setTimer(int i, int duration){
+	timer_flag[i] = 0;
+	timer_counter[i] = duration;
+}
+void timer_run(int i){
+	if(timer_counter[i] > 0){
+		timer_counter[i]--;
+		if(timer_counter[i] <= 0) timer_flag[i] = 1;
+	}
+}
+void timerRun(){
+	timer_run(0);
+	timer_run(1);
+	timer_run(2);
+	timer_run(3);
+}
